e9cf4d082d7b211460329dbbf3804e27dc43c461
devinkwok <devin.kwok@gmail.com>
2025-01-15 04:18:21 -0700

    EXP run a selection of small scale experiments

--- scripts/small_butterfly/small_butterfly.sh
+++ scripts/small_butterfly/small_butterfly.sh
@@ -3,8 +3,8 @@
 # ./scripts/epsilon_search_strategy/epsilon_search_strategy_single.sh 0 0 99 true
 
 for REPLICATE in 1 2; do
-    for SCALE in 0.000001 0.0001 0.01 0.1 1; do
-        for STEP in 0 1950; do
+    for SCALE in 0.000001 0.0001 0.01 1; do
+        for STEP in 0 390; do
             sbatch ./scripts/small_butterfly/small_butterfly_single.sh $STEP $SCALE $REPLICATE resnet8-64 128 0.1 0.02 ".*norm.*" "shallow-wide"
             sbatch ./scripts/small_butterfly/small_butterfly_single.sh $STEP $SCALE $REPLICATE resnet32-16 128 0.1 0.02 ".*norm.*" "deep-narrow"
             sbatch ./scripts/small_butterfly/small_butterfly_single.sh $STEP $SCALE $REPLICATE resnet20-32 128 0.001 0.02 ".*norm.*" "lr-0.001"
@@ -13,7 +13,7 @@ for REPLICATE in 1 2; do
 
             sbatch ./scripts/small_butterfly/small_butterfly_single.sh $STEP $SCALE $REPLICATE resnet20-32 128 0.1 0.02 '^((?!norm).)*$' "norm-only"
             for BLOCK in 0 1 2; do
-                for SUBBLOCK in 0 1 2; do
+                for SUBBLOCK in 0; do
                     sbatch ./scripts/small_butterfly/small_butterfly_single.sh $STEP $SCALE $REPLICATE resnet20-32 128 0.1 0.02 '^((?!block'$BLOCK.$SUBBLOCK'.conv).)*$' "block$BLOCK.$SUBBLOCK-only"
                 done
             done
