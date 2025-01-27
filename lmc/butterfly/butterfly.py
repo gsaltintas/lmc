- Simplify butterfly logging - Fix gradient accumulation (WIP)

lmc/butterfly/butterfly.py
@@ -116,15 +116,17 @@ def get_batch_noise(
 
     # Compute gradients w.r.t the model's parameters
     noise = {}
+    non_noise_layers = []
     for name, param in model.named_parameters():
         grad = param.grad
         if name not in layers:
-            logger.info(f"Not generating any noise for parameter ({name}).")
+            non_noise_layers.append(name)
             noise[name] = torch.zeros_like(param)
         elif grad is None:
             grad = torch.zeros_like(param)
         else:
             noise[name] = grad.clone()
+    logger.info(f"Did not generate any noise for parameters: ({non_noise_layers}).")
     model.zero_grad()
     return noise
 
@@ -152,15 +154,17 @@ def get_gaussian_noise(
     # when adding noise to w, (w+n)*x = wx + xn has scale proportional to w
     # when adding noise to x, w*(x+n) = wx + wn should also have the same scale
     noise_dct = dict()
+    not_perturbed_layers = []
     for name, param in model.named_parameters():
         if name not in layers:
-            logger.info(f"Not generating any noise for parameter ({name}).")
+            not_perturbed_layers.append(name)
             noise_dct[name] = torch.zeros_like(param)
             continue
         noise = torch.empty_like(param)
         with torch.no_grad():
             torch.nn.init.normal_(noise, 0, std[name])
         noise_dct[name] = noise
+    logger.info(f"Did not generate any noise for parameters ({not_perturbed_layers}).")
     model.zero_grad()
     return noise_dct
 
@@ -186,7 +190,8 @@ def perturb_model(
 
 def get_l2(noise: Union[Dict[str, torch.Tensor], torch.Tensor]) -> float:
     if isinstance(noise, dict):
-        noise = parameters_to_vector(noise.values())
+        noise_ = [p.detach().cpu().contiguous() for p in noise.values()]
+        noise = parameters_to_vector(noise_)
     return torch.linalg.norm(noise)
 
 

lmc/experiment/train.py
@@ -263,7 +263,10 @@ class TrainingRunner(ExperimentManager):
         if self.config.data.task_type == TaskType.GENERATION:
             # Language modeling
             outputs = element.model(**batch)
-            loss = outputs.loss
+            # loss = outputs.loss
+            loss = (
+                outputs.loss / self.config.trainer.gradient_accumulation_steps
+            )  # Scale loss
 
         elif self.config.data.task_type in [
             TaskType.CLASSIFICATION,
@@ -272,26 +275,35 @@ class TrainingRunner(ExperimentManager):
         ]:
             # Classification tasks
             outputs = element.model(**batch)
-            loss = outputs.loss
+            # loss = outputs.loss
+            loss = (
+                outputs.loss / self.config.trainer.gradient_accumulation_steps
+            )  # Scale loss
             logits = outputs.logits
         elif self.config.data.task_type == TaskType.QUESTION_ANSWERING:
             # Question answering
             outputs = element.model(**batch)
-            loss = outputs.loss
+            # loss = outputs.loss
+            loss = (
+                outputs.loss / self.config.trainer.gradient_accumulation_steps
+            )  # Scale loss
             start_logits = outputs.start_logits
             end_logits = outputs.end_logits
         elif self.config.data.task_type == TaskType.REGRESSION:
             # Regression tasks (like STS-B)
             outputs = element.model(**batch)
-            loss = outputs.loss
+            # loss = outputs.loss
+            loss = (
+                outputs.loss / self.config.trainer.gradient_accumulation_steps
+            )  # Scale loss
             logits = outputs.logits.squeeze(
                 -1
             )  # Remove last dimension since it's regression
         else:
             raise ValueError(f"Unsupported task type: {self.config.data.task_type}")
 
+        loss.backward()
         if element.curr_step % self.config.trainer.gradient_accumulation_steps == 0:
-            loss.backward()
             if clip_val := self.config.trainer.opt.gradient_clip_val:
                 torch.nn.utils.clip_grad_norm_(element.model.parameters(), clip_val)
             element.opt.step()