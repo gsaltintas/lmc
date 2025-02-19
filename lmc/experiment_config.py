102cc54f732df4e51be9ef56ab7503dbad621d5e
devinkwok <devin.kwok@gmail.com>
2025-02-19 18:13:07 -0500

    FIX add --evaluate_ckpt3

--- lmc/experiment_config.py
+++ lmc/experiment_config.py
@@ -219,6 +219,7 @@ class Experiment:
     zip_and_save_source: bool = True
     evaluate_ckpt1: str = None
     evaluate_ckpt2: str = None
+    evaluate_ckpt3: str = None
     resume_from: Optional[str] = None
     resume_step: Optional[str] = "-1"
     log_to_same_experiment: Optional[bool] = False
@@ -237,6 +238,7 @@ class Experiment:
     _deterministic: str = "If true, make CUDA exactly deterministic."
     _evaluate_ckpt1: str = "Replaces model1 with pretrained checkpoints for evaluation only: a checkpoint must exist for every step in eval_freq or eval_specific_steps"
     _evaluate_ckpt2: str = "Replaces model2 with pretrained checkpoints for evaluation only: a checkpoint must exist for every step in eval_freq or eval_specific_steps"
+    _evaluate_ckpt3: str = "Replaces model3 with pretrained checkpoints for evaluation only: a checkpoint must exist for every step in eval_freq or eval_specific_steps"
     _zip_and_save_source: str = "If true, copy code to output dir and zip compress"
     _resume_from: str = "Directory to load and resume checkpoint or wandb run by specifying wandb:ENTITY/PROJECT/PROJECT_ID"
 
@@ -257,6 +259,7 @@ class Experiment:
         self.resume_step = kwargs.get("resume_step", "-1")
         self.evaluate_ckpt1 = kwargs.get("evaluate_ckpt1", None)
         self.evaluate_ckpt2 = kwargs.get("evaluate_ckpt2", None)
+        self.evaluate_ckpt3 = kwargs.get("evaluate_ckpt3", None)
         self.deterministic = kwargs.get("deterministic", False)
         self.zip_and_save_source = kwargs.get("zip_and_save_source", True)
 
